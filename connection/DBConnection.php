abc6f9c892d6b74571cb7f08c161e2adb2604fcb
Terry Henning <terryjames07@gmail.com>
2017-04-26 23:57:51 -0700

    First database connection file

---
+++ connection/DBConnection.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * Author: Henning, Terry
+ * Date: April 26, 2017
+ * Description: This file contains the DB connection functions
+ */
+class DBConnection {
+
+	private $connection;
+	private $log;
+	
+	/**
+	 * This is the default contructor 
+	 * @param unknown $db The database name 
+	 */
+	function __construct($db) {
+		
+		try { 
+			
+			$this->connection = new PDO("mysql:host=localhost;dbname=$db", "deles", "Delesjh1");
+			
+		} catch(Exception $e) {
+			
+			die($e->getMessage());
+		}
+	}
+	
+	/**
+	 * 
+	 * @param unknown $sql
+	 * @return number
+	 */
+	public function select_statement($sql) {
+		
+		$result= 0;
+		
+		try {
+			
+			$stmt = $this->connection->query($sql);
+			$result = $stmt->fetchAll();
+				
+		} catch (Exception $e) {
+				
+			echo $e->getMessage();
+			
+			error_log($e->getMessage());
+		}
+		
+		return $result;
+		
+	}
+	
+	/**
+	 * 
+	 * @param unknown $sql
+	 */
+	public function insert_statement($sql) {
+		
+		$rows = 0;
+		
+		try {
+			
+			$this->connection->beginTransaction();
+			
+			$stm = $this->connection->prepare($sql);
+			$rows = $stm->execute();
+			
+			$this->connection->commit();
+			
+		} catch (Exception $e) {
+			
+			echo $e->getMessage();
+			
+			error_log($e->getMessage());
+			
+			$this->connection->rollback();
+		}
+		
+		return $rows;
+	}
+	
+	/**
+	 *
+	 * @param unknown $sql
+	 */
+	public function update_statement($sql) {
+	
+		return insert($sql);
+	}
+}
+ 
